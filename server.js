209054713deb9f0454cef60805c65c17e1c88880
Kevin Zheng <42kevinzheng@gmail.com>
2021-07-03 19:35:52 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -2,27 +2,27 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import data from './data.js';
-import userRouter from './routes/userRouter.js';
-import productRouter from './routes/productRoute.js';
+import productRouter from './routers/productRouter.js';
+import userRouter from './routers/userRouter.js';
+import orderRouter from './routers/orderRouter.js';
 
 dotenv.config();
 
 const app = express();
-app.use(express.json({ limit: '30mb', extended: true }))
-app.use(express.urlencoded({ limit: '30mb', extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use('/api/users', userRouter);
 app.use('/api/products', productRouter);
-app.use((err, req, res, next) => {
-  res.status(500).send({ message: err.message });
-});
-
+app.use('/api/orders', orderRouter);
 app.get('/', (req, res) => {
   res.send('Server is ready');
 });
 
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
 const PORT = process.env.PORT|| 5001;
 
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
