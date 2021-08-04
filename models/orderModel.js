35873a1caaece1955607eeaa2f68feaaf6df7277
Kevin Zheng <42kevinzheng@gmail.com>
2021-08-03 22:08:07 -0400

    Add files via upload

--- models/orderModel.js
+++ models/orderModel.js
@@ -16,7 +16,10 @@ const orderSchema = new mongoose.Schema(
       },
     ],
     shippingAddress: {
-      fullName: { type: String, required: true },
+      firstName: { type: String, required: true },
+      lastName: { type: String, required: true },
+      email: { type: String, required: true },
+      phone: { type: String, required: true },
       address: { type: String, required: true },
       city: { type: String, required: true },
       postalCode: { type: String, required: true },
