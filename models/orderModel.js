9839328851d8d53f8d09240957324bb8e1e08062
Kevin Zheng <42kevinzheng@gmail.com>
2021-08-12 23:12:41 -0400

    Add files via upload

--- models/orderModel.js
+++ models/orderModel.js
@@ -22,6 +22,7 @@ const orderSchema = new mongoose.Schema(
       phone: { type: String, required: true },
       address: { type: String, required: true },
       city: { type: String, required: true },
+      state: { type: String, required: true },
       postalCode: { type: String, required: true },
       country: { type: String, required: true },
       lat: Number,
